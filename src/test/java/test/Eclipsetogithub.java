daf2823e2a05bd7a0110c0d9306ceb10ba78aa4b
samimmanvel <samimmanvelj@gmail.com>
2024-06-09 18:34:57 +0530

    initial commit

---
+++ src/test/java/test/Eclipsetogithub.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Eclipsetogithub {
+
+	private void tc1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void tc2() {
+		// TODO Auto-generated method stub
+
+	}
+	private void tc3() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
